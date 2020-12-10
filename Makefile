3bb351f11ee7857764103b59481958b8bf839506
weidong.fan <fwdssg@gmail.com>
2020-12-10 23:11:58 +0800

    Symbolic links

--- Makefile
+++ Makefile
@@ -172,6 +172,7 @@ UPROGS=\
 	$U/_sh\
 	$U/_stressfs\
 	$U/_usertests\
+	$U/_symlinktest\
 	$U/_grind\
 	$U/_wc\
 	$U/_zombie\
--- kernel/sysfile.c
+++ kernel/sysfile.c
@@ -309,7 +309,31 @@ sys_open(void)
       return -1;
     }
     ilock(ip);
-    if(ip->type == T_DIR && omode != O_RDONLY){
+
+    if (!(omode & O_NOFOLLOW)) {
+      int depth = 10;
+      for (; ip->type == T_SYMLINK && depth; depth--) {
+        if((ip->size > MAXPATH) || (readi(ip, 0, (uint64)path, 0, ip->size) != ip->size)){
+          iunlockput(ip);
+          end_op();
+          return -1;
+        }
+        iunlockput(ip);
+
+        if((ip = namei(path)) == 0){
+          end_op();
+          return -1;
+        }
+        ilock(ip);
+      }
+      if(depth == 0) {
+        iunlockput(ip);
+        end_op();
+        return -1;
+      }
+    }
+
+    if (ip->type == T_DIR && omode != O_RDONLY){
       iunlockput(ip);
       end_op();
       return -1;
@@ -341,7 +365,7 @@ sys_open(void)
   f->readable = !(omode & O_WRONLY);
   f->writable = (omode & O_WRONLY) || (omode & O_RDWR);
 
-  if((omode & O_TRUNC) && ip->type == T_FILE){
+  if((omode & O_TRUNC) && (ip->type == T_FILE || ip->type == T_SYMLINK)){
     itrunc(ip);
   }
 
@@ -482,5 +506,25 @@ sys_pipe(void)
     fileclose(wf);
     return -1;
   }
+  return 0;
+}
+
+uint64
+sys_symlink(void){
+  char target[MAXPATH], path[MAXPATH];
+  struct inode *ip;
+
+  if(argstr(0, target, MAXPATH) < 0 || argstr(1, path, MAXPATH) < 0)
+    return -1;
+
+  begin_op();
+  if((ip = create(path, T_SYMLINK, 0, 0)) == 0){
+    end_op();
+    return -1;
+  }
+  if(writei(ip, 0, (uint64)target, 0, strlen(target) + 1) <= 0)
+    panic("sys_symlink: writei");
+  iunlockput(ip);
+  end_op();
   return 0;
 }
